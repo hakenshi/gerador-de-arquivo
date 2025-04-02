a167ef6c1d5df2b15382f14c7895b078d0053446
hakenshi <fkdias025@gmail.com>
2025-04-02 10:14:15 -0300

    updated prisma schema

---
+++ prisma/migrations/20250402131340_updated_all_tables_names/migration.sql
@@ -0,0 +1,40 @@
+/*
+  Warnings:
+
+  - You are about to drop the `documents` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "documents";
+
+-- DropTable
+DROP TABLE "users";
+
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "secondName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Documents" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_id_key" ON "Users"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Documents_id_key" ON "Documents"("id");
